38b1c7c3a81fb428417fb8d79f3c72de20c0a04e
JeroenE2000 <jmh.elst@student.avans.nl>
2024-06-24 18:05:49 +0200

    MovieService en TMDBService
    It is now possible to add movies to the database by simple using the following url in postman:
    http://localhost:8000/api/movies/import?totalPages=15
    
    15 indicates the totalpages you want to intergrate so now it will take the first 15 pages but if you say i want 20 it takes 20 if they already exists no duplicate values will be added

---
+++ app/Repositories/MovieRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Movie;
+use App\Interface\TMDBRepositoryInterface;
+
+class MovieRepository implements TMDBRepositoryInterface
+{
+
+    public function findById($tmdbId)
+    {
+        return Movie::where('tmdb_id', $tmdbId)->first();
+    }
+
+    public function create(array $movieData)
+    {
+        return Movie::create($movieData);
+    }
+}
---
+++ app/Services/MovieService.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\MovieRepository;
+
+class MovieService
+{
+    protected $tmdbService;
+    protected $movieRepository;
+
+    public function __construct(TMDBService $tmdbService, MovieRepository $movieRepository)
+    {
+        $this->tmdbService = $tmdbService;
+        $this->movieRepository = $movieRepository;
+    }
+
+    public function importMoviesFromTMDB($totalPages = 10)
+    {
+        $movies = $this->tmdbService->fetchAllMovies($totalPages);
+        $importedCount = 0;
+
+        foreach ($movies as $movieData) {
+            $existingMovie = $this->movieRepository->findById($movieData['id']);
+            if (!$existingMovie) {
+                $this->movieRepository->create([
+                    'tmdb_id' => $movieData['id'],
+                    'title' => $movieData['title'],
+                    'overview' => $movieData['overview'],
+                    'release_date' => $movieData['release_date'],
+                    'poster_path' => $movieData['poster_path'],
+                ]);
+                $importedCount++;
+            }
+        }
+
+        return $importedCount;
+    }
+}
